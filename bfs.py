0a765c2ea624d49f1b16ecbf2985cabec180cf76
Sayjal <ssayjal11@gmail.com>
2023-07-21 07:25:34 +0545

    BFS problem solved

---
+++ bfs.py
@@ -0,0 +1,30 @@
+import queue 
+adj_list={
+    "a":["b","c","d"],
+    "b":["e","f"],
+    "c":["g","h"],
+    "d":["i"],
+    "e":[],
+    "f":[],
+    "g":[],
+    "h":[],
+    "i":[],
+}
+
+visited=[],
+queue=[],
+traversed_output=[];
+def bfs(visited,graoh,node):
+    visited.append(node)
+    queue.sppend(node)
+while queue:
+    m=queue.pop(0);
+    traversed_output.append(m);
+    for (neighbor in graph[m]):
+        if neighbor not in visited:
+            visited.append(neighbor);
+            queue.append(neighbor);
+print("our traverse output=".traversed_output);
+print("Breadth First Search");
+rootnode=input("Enter the root node: ");
+bfs(visited,adj_list,rootnode);
